27689952e4deab3c2bc2060b7f6ec24675af3348
Wisnukan <28.ngakanmadewisnumahesaadnyana.com>
2024-11-02 16:41:57 +0800

    fix

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,19 @@
 import express from "express";
 import { auth_controller } from "../controllers/auth/index.js";
 import callbackHandle from "../utils/callbackHandle.js";
+import User from "../databases/models/user.model.js";
+import { hashPassword } from "../utils/bcrypt.js";
 
 const router = express.Router();
 const { login_user, change_password } = auth_controller;
 
 router.post("/login", callbackHandle(login_user));
 router.put("/change-password", callbackHandle(change_password));
+router.put("/create-admin", async (req, res) => {
+  await User.create({
+    number_id: "admin",
+    password: await hashPassword("admin"),
+  });
+});
 
 export default router;
